0074133c05656f9fa736ff3710862e8290ad96a4
schwasam <anonymous>
2023-05-22 22:12:12 +0200

    add lexer up to page 20

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,8 +1,8 @@
 const std = @import("std");
 
-const Token = union(enum) {
+pub const Token = union(enum) {
     // special
-    illegal: void,
+    illegal: u8,
     eof: void,
 
     // identifiers and literals
@@ -26,22 +26,28 @@ const Token = union(enum) {
     let: void,
 };
 
-const Lexer = struct {
+pub const Lexer = struct {
     const Self = @This();
-    input: []const u8 = undefined,
+    const Keywords = std.ComptimeStringMap(Token, .{ .{ "fn", .function }, .{ "let", .let } }); // keywords
+
+    input: []const u8 = undefined, // input / program code
     position: usize = 0, // current position in input (points to current char)
     read_position: usize = 0, // current reading position in input (after current char)
     ch: u8 = undefined, // current char under examination
 
     pub fn init(input: []const u8) Self {
-        var lexer = Self{ .input = input };
-        readChar(&lexer);
+        var self = Self{ .input = input };
+        readCharacter(&self);
 
-        return lexer;
+        return self;
     }
 
     pub fn nextToken(self: *Self) Token {
-        var token: Token = .illegal;
+        var skipRead = false;
+
+        skipWhitespace(self);
+
+        var token: Token = undefined;
         token = switch (self.ch) {
             '=' => .assign,
             '+' => .plus,
@@ -52,14 +58,39 @@ const Lexer = struct {
             '{' => .lbrace,
             '}' => .rbrace,
             0 => .eof,
-            else => .illegal,
+            else => |c| {
+                if (isLetter(c)) {
+                    var id = readIdentifier(self);
+                    var tk = lookupIdentifier(id);
+                    skipRead = true;
+                    return tk;
+                }
+
+                if (isDigit(c)) {
+                    var num = readNumber(self);
+                    var tk = Token{ .int = num };
+                    skipRead = true;
+                    return tk;
+                }
+
+                return Token{ .illegal = c };
+            },
         };
-        readChar(self);
+
+        if (!skipRead) {
+            readCharacter(self);
+        }
 
         return token;
     }
 
-    pub fn readChar(self: *Self) void {
+    pub fn skipWhitespace(self: *Self) void {
+        while (isWhitespace(self.ch)) {
+            readCharacter(self);
+        }
+    }
+
+    pub fn readCharacter(self: *Self) void {
         if (self.read_position >= self.input.len) {
             self.ch = 0;
         } else {
@@ -68,32 +99,108 @@ const Lexer = struct {
         self.position = self.read_position;
         self.read_position += 1;
     }
+
+    pub fn readIdentifier(self: *Self) []const u8 {
+        var start_idx = self.position;
+        while (isLetter(self.ch)) {
+            readCharacter(self);
+        }
+        var end_idx = self.position;
+
+        return self.input[start_idx..end_idx];
+    }
+
+    pub fn lookupIdentifier(identifier: []const u8) Token {
+        var keyword = Keywords.get(identifier);
+        if (keyword == null) {
+            return Token{ .ident = identifier };
+        }
+
+        return keyword.?;
+    }
+
+    pub fn readNumber(self: *Self) i64 {
+        var start_idx = self.position;
+        while (isDigit(self.ch)) {
+            readCharacter(self);
+        }
+        var end_idx = self.position;
+        var number = std.fmt.parseInt(i64, self.input[start_idx..end_idx], 10) catch -1;
+
+        return number;
+    }
+
+    pub fn isWhitespace(ch: u8) bool {
+        return (ch == ' ') or (ch == '\t') or (ch == '\n') or (ch == '\r');
+    }
+
+    pub fn isLetter(ch: u8) bool {
+        return ('a' <= ch and ch <= 'z') or ('A' <= ch and ch <= 'Z') or (ch == '_');
+    }
+
+    pub fn isDigit(ch: u8) bool {
+        return ('0' <= ch and ch <= '9');
+    }
 };
 
-test "lexer test: =+(){},;" {
+test "lexer test 1" {
     const input = "=+(){},;";
     const expected_tokens = [_]Token{ .assign, .plus, .lparen, .rparen, .lbrace, .rbrace, .comma, .semicolon, .eof };
 
     var lexer = Lexer.init(input);
-    var index: usize = 0;
-    while (index < expected_tokens.len) : (index += 1) {
-        var token = lexer.nextToken();
-        var expected_token = expected_tokens[index];
 
-        try std.testing.expectEqual(expected_token, token);
+    for (expected_tokens) |expected_token| {
+        var token = lexer.nextToken();
+
+        try std.testing.expectEqualDeep(expected_token, token);
     }
 }
 
-test "lexer test: ====)));" {
+test "lexer test 2" {
     const input = "====)));";
     const expected_tokens = [_]Token{ .assign, .assign, .assign, .assign, .rparen, .rparen, .rparen, .semicolon, .eof };
 
     var lexer = Lexer.init(input);
-    var index: usize = 0;
-    while (index < expected_tokens.len) : (index += 1) {
-        var token = lexer.nextToken();
-        var expected_token = expected_tokens[index];
 
-        try std.testing.expectEqual(expected_token, token);
+    for (expected_tokens) |expected_token| {
+        var token = lexer.nextToken();
+
+        try std.testing.expectEqualDeep(expected_token, token);
     }
 }
+
+test "lexer test 3" {
+    const input =
+        \\let five = 5;
+        \\let ten = 10;
+        \\
+        \\let add = fn(x, y) {
+        \\  x + y;
+        \\};
+        \\
+        \\let result = add(five, ten);
+    ;
+    try std.testing.expect(input[0] == 'l');
+
+    // zig fmt: off
+    const expected_tokens = [_]Token{ 
+        .let, Token{ .ident = "five" }, .assign, Token{ .int = 5 }, .semicolon, // let five = 5;
+        .let, Token{ .ident = "ten" }, .assign, Token{ .int = 10 }, .semicolon, // let ten = 10;
+        .let, Token{ .ident= "add" }, .assign, .function, .lparen, Token{ .ident = "x" }, .comma, Token{ .ident = "y" }, .rparen, .lbrace, // let add = fn(x, y) {
+        Token{ .ident = "x" }, .plus, Token{ .ident = "y" }, .semicolon, // x + y;
+        .rbrace, .semicolon, // };
+        .let, Token{ .ident = "result" }, .assign, Token{ .ident = "add" }, .lparen, Token{ .ident = "five" }, .comma, Token{ .ident = "ten" }, .rparen, .semicolon, // let result = add(five, ten);
+        .eof
+    };
+    // zig fmt: on
+
+    var lexer = Lexer.init(input);
+
+    for (expected_tokens) |expected_token| {
+        var token = lexer.nextToken();
+
+        try std.testing.expectEqualDeep(expected_token, token);
+    }
+}
+
+// TODO: continue with page 21ff
--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,51 @@
 const std = @import("std");
+const lex = @import("lexer.zig");
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
     const stdout_file = std.io.getStdOut().writer();
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    try stdout.print("All your {s} are belong to us.\n", .{"tokens"});
 
-    try bw.flush(); // don't forget to flush!
-}
+    const input =
+        \\let five = 5;
+        \\let ten = 10;
+        \\
+        \\let add = fn(x, y) {
+        \\  x + y;
+        \\};
+        \\
+        \\let result = add(five, ten);
+    ;
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    // zig fmt: off
+    const expected_tokens = [_]lex.Token{ 
+        .let, lex.Token{ .ident = "five" }, .assign, lex.Token{ .int = 5 }, .semicolon, // let five = 5;
+        .let, lex.Token{ .ident = "ten" }, .assign, lex.Token{ .int = 10 }, .semicolon, // let ten = 10;
+        .let, lex.Token{ .ident= "add" }, .assign, .function, .lparen, lex.Token{ .ident = "x" }, .comma, lex.Token{ .ident = "y" }, .rparen, .lbrace, // let add = fn(x, y) {
+        lex.Token{ .ident = "x" }, .plus, lex.Token{ .ident = "y" }, .semicolon, // x + y;
+        .rbrace, .semicolon, // };
+        .let, lex.Token{ .ident = "result" }, .assign, lex.Token{ .ident = "add" }, .lparen, lex.Token{ .ident = "five" }, .comma, lex.Token{ .ident = "ten" }, .rparen, .semicolon, // let result = add(five, ten);
+        .eof
+    };
+    // zig fmt: on
+
+    try stdout.print("input:\n------\n{s}\n------\n", .{input});
+
+    var lexer = lex.Lexer.init(input);
+
+    var index: usize = 0;
+    while (index < expected_tokens.len) : (index += 1) {
+        var token = lexer.nextToken();
+        var expected_token = expected_tokens[index];
+
+        switch (token) {
+            .ident => try stdout.print("token: {}, ident: {s}, expected: {}\n", .{ token, token.ident, expected_token }),
+            .illegal => try stdout.print("token: {}, illegal: {}, expected: {}\n", .{ token, token.illegal, expected_token }),
+            else => try stdout.print("token: {}, expected: {}\n", .{ token, expected_token }),
+        }
+    }
+
+    try bw.flush();
 }
